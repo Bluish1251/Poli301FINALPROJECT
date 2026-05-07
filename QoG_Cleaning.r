e0e0b894fdf4d7fa72d3ffb8c36e5c9e7fb4f571
Your Name <cm6of6@gmail.com>
2026-05-07 00:45:57 -0400

    ttt

--- QoG_Cleaning.r
+++ QoG_Cleaning.r
@@ -1,20 +1,19 @@
 library(tidyverse)
-
 qog <- read.csv("poli301dataset.csv")
 
 qog_clean <- qog %>%
-  filter(year >= 1990 & year <= 2025) %>% # timeline span
+  filter(year >= 1990 & year <= 2025) %>% # only keeps rows with year cond met & dropping blank column
   drop_na(al_ethnic2000, p_polity2, wdi_gdpcapcon2015, wdi_pop)
 
 qog_clean <- qog_clean %>%
-  mutate(
+  mutate( # adding new columns with log transformed values (for scewed values)
     log_gdp = log(wdi_gdpcapcon2015),
-    log_pop = log(wdi_pop) #log transformation 
+    log_pop = log(wdi_pop)  
   )
 
 nrow(qog_clean)
-summary(qog_clean)
+summary(qog_clean) #testing R functions
 
-write.csv(qog_clean, "qog_cleaned2.csv", row.names = FALSE)
+#write.csv(qog_clean, "qog_cleaned2.csv", row.names = FALSE)
 
-# still need to do work for dv
+# dv on file 'qog_unga.r'
